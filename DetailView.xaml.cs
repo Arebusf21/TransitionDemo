ec3222603dc24178c25d4c55b4ad1f5b46716100
Allan Burge <burgea@shernet.sheridancollege.ca>
2021-07-11 11:20:52 -0400

    Add project files.

---
+++ DetailView.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+using muxc=Microsoft.UI.Xaml.Controls;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TransitionDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DetailView : Page
+    {
+
+#region Field & Object Definitions
+
+        Uri imageUri;   // Image to view.
+
+#endregion
+
+        #region Property Definitions
+        float imageActualHeight { get; set; }
+        float imageActualWidth { get; set; }
+        #endregion
+
+        #region Methods
+
+        public DetailView()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Called by the event handler for the NavigationView.BackRequested event.
+        /// </summary>
+        /// <returns></returns>
+        private bool On_BackRequested()
+        {
+
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (!rootFrame.CanGoBack)
+                return false;
+
+            rootFrame.GoBack(new SuppressNavigationTransitionInfo());     // Go back to the MainPage.
+
+            return true;
+        }
+
+        /// <summary>
+        /// Fit the image to the screen size, (Viewport).
+        /// </summary>
+        public void FitToScreen()
+        {
+            float zoomFactor = (float)Math.Min(this.scrollViewer.ActualWidth / this.imageToShow.ActualWidth,
+                this.scrollViewer.ActualHeight / this.imageToShow.ActualHeight);
+            scrollViewer.ChangeView(null, null, zoomFactor);
+            return;
+        }
+
+        /// <summary>
+        /// Sow the image in the ScrollViewer at actual size.
+        /// </summary>
+        private void ShowActualSize()
+        {
+            scrollViewer.ChangeView(null, null, 1);
+            return;
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Handles the BackButton click event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            On_BackRequested();
+        }
+
+        /// <summary>
+        /// Overrides the OnNavigatedTo event of the DetailView Page.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            imageUri = (Uri)e.Parameter;    // Uri from the selected image in the GridView of the MainPage.
+
+            ConnectedAnimation animation = ConnectedAnimationService.GetForCurrentView().GetAnimation("forwardAnimation");
+            if (animation != null)
+            {
+                animation.TryStart(imageToShow);
+            }
+        }
+
+        /// <summary>
+        /// Overrides the OnNavigatingFrom event of the DetailView Page.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+
+            if (e.NavigationMode == NavigationMode.Back)
+            {
+                ConnectedAnimation animation = ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("backAnimation", imageToShow);
+
+                // Use the recommended configuration for back animation.
+                animation.Configuration = new DirectConnectedAnimationConfiguration();
+            }
+        }
+
+        /// <summary>
+        /// Handles the ZoomSlider ValueChanged event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ZoomSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            if (scrollViewer != null)
+            {
+                scrollViewer.ChangeView(null, null, (float)e.NewValue);
+            }
+        }
+        
+      
+        /// <summary>
+        /// Handles the ScrollViewer loaded event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ScrollViewer_Loaded(object sender, RoutedEventArgs e)
+        {
+            FitToScreen();
+            return;
+        }
+
+        #endregion
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+using muxc = Microsoft.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+/// <summary>
+/// Used to demo a technique for displaying a photo or other image from a GridView to a image view.
+/// Demonstrates Connected Animation from a GridView to an image and back.
+/// Based on: https://docs.microsoft.com/en-us/windows/apps/design/motion/connected-animation
+/// 
+/// </summary>
+namespace TransitionDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        #region Field & Object Definitions
+        GridViewItem gridViewItem = null;
+
+        #endregion
+
+        #region Property Definitions
+        #endregion
+
+        #region Methods
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = new GridViewModel();
+        }
+        #endregion
+
+        #region Event Handlers  
+
+        /// <summary>
+        /// Overrides the OnNavigatedTo Page event. Not used.
+        /// </summary>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
+
+        /// <summary>
+        /// Handles the GridView loaded event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void GridViewDemo_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            // handle the back animation if returning from the detail view.
+            GridViewItem item = this.gridViewItem; // Get persisted item
+            if (item != null)
+            {
+                GridViewDemo.ScrollIntoView(item);
+                ConnectedAnimation animation =
+                    ConnectedAnimationService.GetForCurrentView().GetAnimation("backAnimation");
+                if (animation != null)
+                {
+                    await GridViewDemo.TryStartConnectedAnimationAsync(animation, item, "imageView");
+                }
+            }
+            return;
+        }
+
+        /// <summary>
+        /// Handles the GridView DoubleTapped event. We will use the double tap to simulate a double click event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GridViewDemo_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            // Save the selected item.
+            this.gridViewItem = (GridViewItem)GridViewDemo.SelectedItem;
+
+            // Prepare the animation
+            GridViewDemo.PrepareConnectedAnimation("forwardAnimation", this.gridViewItem, "imageView");
+
+
+            // Pass the image uri to the DetailView page.
+            Uri item = this.gridViewItem.ImageUri;
+            this.Frame.Navigate(typeof(DetailView), item, new SuppressNavigationTransitionInfo());
+            return;
+        }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// GridViewItem class. Represents an Item in the GridView.
+    /// </summary>
+    class GridViewItem
+    {
+        public Uri ImageUri { get; set; }       // Uri if the image.
+        public string Txt { get; set; }         // Text id.
+
+        /// <summary>
+        /// Create a GridViewItem based on a Uri and id.
+        /// </summary>
+        /// <param name="imageUri"></param>
+        /// <param name="str"></param>
+        public GridViewItem(Uri imageUri, string str)
+        {
+            ImageUri = imageUri;
+            Txt = str;
+        }
+
+        /// <summary>
+        /// Default creator.
+        /// </summary>
+        public GridViewItem() { return; }
+    }
+
+    /// <summary>
+    /// GridViewModel class. Represents the collection of items in the GridView 
+    /// </summary>
+    class GridViewModel
+    {
+        public List<GridViewItem> Items { get; set; }
+
+        /// <summary>
+        /// Note that this will only work as long as the images are added to the project. Build action set to content.
+        /// </summary>
+        public GridViewModel()
+        {
+            //Dummy Data
+            Items = new List<GridViewItem>();
+            Items.Add(new GridViewItem() { ImageUri = new Uri("ms-appx:///Assets/19.JPG"), Txt = "1" });
+            Items.Add(new GridViewItem() { ImageUri = new Uri("ms-appx:///Assets/21.JPG"), Txt = "2" });
+        }
+    }
+}
