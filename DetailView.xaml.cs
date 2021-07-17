2739c9c9fe7d5a6a4b5fa1a5ac8d23d472a06baa
Allan Burge <burgea@shernet.sheridancollege.ca>
2021-07-17 14:18:09 -0400

    Added scrollling on Zooming

--- DetailView.xaml.cs
+++ DetailView.xaml.cs
@@ -11,20 +11,27 @@ using System.Timers;
 namespace TransitionDemo
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// Based on an empty page that can be used on its own or navigated to within a Frame.
+    /// There appears to be a bug in the ScrollViewer control, the ZoomFactor property is not been updated ater a call to ChangeView()
+    /// To work around this, we are keeping track of the zoomFactor in a local field. At least this is the case when an image is enclosed in 
+    /// a ScrollViewer. The ScrollViewer control wants to display the image based on the width. It tries to fit the image width to the viewport and 
+    /// scrolls the length if needed, it also sets the ScrollFactor to 1. While this is OK for an image where the width is greater than the length, it 
+    /// doesn't work for a long image, (where the length is longer than the width).
+    /// Using the code below, when trying to show the image full size, it will still scroll the width of the image even though there is room 
+    /// to fit int in. Frustrating....
+    /// Also, when zooming an image, right now you will have no scrolling unless its turned on for zooming. We need to be able to turn zooming on right
+    /// after launch. We have provided a fix of sorts but its very much a hack. It looks as though we have a working solution anyway.
     /// </summary>
     public sealed partial class DetailView : Page
     {
 
     #region Field & Object Definitions
-
-        Uri imageUri;   // Image to view.
-
+        private Uri imageUri;   // Image to view.
+        private float zoomFactor;
+        private bool scrollingTurnedOn;
     #endregion
 
     #region Property Definitions
-        float imageActualHeight { get; set; }
-        float imageActualWidth { get; set; }
     #endregion
 
     #region Methods
@@ -51,6 +58,7 @@ namespace TransitionDemo
 
         /// <summary>
         /// Sets the ScrollViewer mode and visibility.
+        /// Not used at present, for future use.
         /// </summary>
         /// <param name="mode"></param>
         /// <param name="visibility"></param>
@@ -67,37 +75,69 @@ namespace TransitionDemo
         }
 
         /// <summary>
+        /// Sets the ScrollViewer ScrollBar visibility.
+        /// </summary>
+        /// <param name="visibility"></param>
+        private void SetScrollViewerVisibility(ScrollBarVisibility visibility)
+        {
+            if (this.scrollViewer != null)
+            {
+                this.scrollViewer.HorizontalScrollBarVisibility = visibility;   
+                this.scrollViewer.VerticalScrollBarVisibility = visibility;
+            }
+            return;
+        }
+
+        /// <summary>
+        /// Determine the ZoomFactor to fit the image to the ScrollViewer viewport.
+        /// not used at present.
+        /// </summary>
+        /// <returns></returns>
+        private float GetZoomFactorToFit()
+        {
+            return (float)Math.Min(this.scrollViewer.ActualWidth / this.imageToShow.ActualWidth,
+                         this.scrollViewer.ActualHeight / this.imageToShow.ActualHeight);
+        }
+
+        /// <summary>
+        /// Determine the Zoom factor to show the image full size in the ScrollViewer.
+        /// </summary>
+        /// <returns></returns>
+        private float GetZoomFactorForFullSize()
+        {
+            // Get the Zoom factor to show the image full size.
+            return (float)Math.Max(this.bitmapImage.PixelWidth / this.imageToShow.ActualWidth,
+                    this.bitmapImage.PixelHeight / this.imageToShow.ActualHeight);
+        }
+
+
+        /// <summary>
         /// Fit the image to the screen size, (Viewport).
         /// </summary>
         public void FitToScreen()
         {
-            //float zoomFactor = (float)Math.Min(this.scrollViewer.ActualWidth / this.imageToShow.ActualWidth,
-            //    this.scrollViewer.ActualHeight / this.imageToShow.ActualHeight);
-            //scrollViewer.ChangeView(null, null, zoomFactor);
-
-            // May not need to do this once the image is initially loaded.....
-            SetScrollViewerMode(ScrollMode.Disabled, ScrollBarVisibility.Disabled);
+            // Get & save vthe zoom factor to fit the image to the screen and update the view.           
+            this.ZoomSlider.Value = this.zoomFactor = GetZoomFactorToFit();
+            this.scrollViewer.ChangeView(null, null, this.zoomFactor);
 
             return;
         }
 
         /// <summary>
-        /// Sow the image in the ScrollViewer at actual size.
+        /// Show the image in the ScrollViewer at its actual size.
         /// </summary>
         private void ShowActualSize()
         {
-            //scrollViewer.ChangeView(null, null, 1);
+            // Enable scrolling.
+            if (!this.scrollingTurnedOn)
+                SetScrollViewerVisibility(ScrollBarVisibility.Auto);
 
             // Set the Zoom factor to show full size and update the view.
-            float zoomFactor = (float)Math.Max(this.bitmapImage.PixelWidth / this.imageToShow.ActualWidth,
-                    this.bitmapImage.PixelHeight / this.imageToShow.ActualHeight);
-            scrollViewer.ChangeView(null, null, zoomFactor);
-
-            // Set the Zoom slider to the new value.
-            ZoomSlider.Value = zoomFactor;
-
-            // Enable scrolling, (May turn scrolling on once the image has been loaded and initially shown).
-            SetScrollViewerMode(ScrollMode.Enabled, ScrollBarVisibility.Auto);
+            // Note that changing the scrollbar visibility will regenerate the image and show it full size even without
+            // calling ChangeView. I am not sure this is the correct behaviour but for now we will work with it.
+            // Anyway, this seems to work ok.
+            this.ZoomSlider.Value = this.zoomFactor = GetZoomFactorForFullSize();
+            this.scrollViewer.ChangeView(null, null, this.zoomFactor);
 
             return;
         }
@@ -122,14 +162,15 @@ namespace TransitionDemo
         /// <param name="e"></param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            base.OnNavigatedTo(e);
+            base.OnNavigatedTo(e);  // Not sure if this should be called first or last?
 
-            imageUri = (Uri)e.Parameter;    // Uri from the selected image in the GridView of the MainPage.
+            this.imageUri = (Uri)e.Parameter;    // Uri from the selected image in the GridView of the MainPage.
 
+            // Start the animation
             ConnectedAnimation animation = ConnectedAnimationService.GetForCurrentView().GetAnimation("forwardAnimation");
             if (animation != null)
             {
-                animation.TryStart(imageToShow);
+                animation.TryStart(this.imageToShow);
             }
         }
 
@@ -141,9 +182,10 @@ namespace TransitionDemo
         {
             base.OnNavigatingFrom(e);
 
+            // initialize the back navigation.
             if (e.NavigationMode == NavigationMode.Back)
             {
-                ConnectedAnimation animation = ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("backAnimation", imageToShow);
+                ConnectedAnimation animation = ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("backAnimation", this.imageToShow);
 
                 // Use the recommended configuration for back animation.
                 animation.Configuration = new DirectConnectedAnimationConfiguration();
@@ -151,41 +193,60 @@ namespace TransitionDemo
         }
 
         /// <summary>
-        /// Handles the ZoomSlider ValueChanged event.
+        /// Handles the ZoomSlider ValueChanged event. Some of the code below is in response to the behaviour of the Zoom control.
+        /// Clicking on the Zoom control, (AppBar Button) causes the Slider ValueChanged event to be fired. this not desirable
+        /// straight after launch as we have just shown an image to fit inside the viewport. Turning Scrolling on will resize the image
+        /// before we are ready for it. We compare the stored zoom factor to the chaned event parameter to see if its changed. 
+        /// It will only be changed if we have moved the slider.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void ZoomSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
-            if (scrollViewer != null)
+            if (this.scrollViewer != null)
             {
-                scrollViewer.ChangeView(null, null, (float)e.NewValue);
+                if (e.NewValue != this.zoomFactor)
+                {
+                    this.scrollViewer.ChangeView(null, null, (float)e.NewValue);
+                    this.zoomFactor = (float)e.NewValue;    //Save the new zoom factor.   
+                    if (!this.scrollingTurnedOn)
+                    {
+                        SetScrollViewerVisibility(ScrollBarVisibility.Auto);
+                        this.scrollingTurnedOn = true;
+                    }
+                }
+                
             }
         }
         
         /// <summary>
         /// Handles the ScrollViewer loaded event.
+        /// Not used at present.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void ScrollViewer_Loaded(object sender, RoutedEventArgs e)
         {
+            // Set the initial value for the zoomFactor, will be set to 1, although in my view it should not be.
+            this.zoomFactor = GetZoomFactorToFit();
+            
             return;
         }
 
         /// <summary>
         /// Handles the Slider Loaded event. 
-        /// Set the initial vale for the slider.
+        /// Set the initial value for the slider.
+        /// Every time the Zoom button is selected, the Slider loaded event is raised.
+        /// So we need to initialize it to the current zoom factor.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void ZoomSlider_Loaded(object sender, RoutedEventArgs e)
         {
             Slider slider = sender as Slider;
-            slider.Value = 1;
+            slider.Value = this.zoomFactor;
         }
 
-    #endregion
-
+        #endregion
     }
 }
